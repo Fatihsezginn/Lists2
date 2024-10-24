af6ad8b915011e1ee02c65f0f3845969b67321ad
pc <pc@DESKTOP-78M62IA>
2024-10-24 21:30:10 +0300

    Add project files.

---
+++ Lists2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> kahveler = new List<string>();
+
+        // Kullanıcıdan 5 kahve ismi alma
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Lütfen {0}. kahve ismini girin: ", i + 1);
+            string kahve = Console.ReadLine();
+            kahveler.Add(kahve);
+        }
+
+        // Kahve listesini foreach döngüsü ile yazdırma
+        Console.WriteLine("\nKahve Listesi:");
+        foreach (string kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
